9b200bc70ce0fb74883bbf7635cf8ee4e6f51ef9
Misha <20xim04@gmail.com>
2023-04-03 16:31:58 +0300

    add crow

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
-#include<iostream>
-int main(){
+#include <curl/curl.h>
+#include <iostream>
+static size_t WriteCallback(void* contents,
+                            size_t size,
+                            size_t nmemb,
+                            void* userp) {
+  return size * nmemb;
+}
 
-    return 0;
+int main() {
+  CURL* curl;
+  CURLcode res;
+  char* readtest = NULL;
+
+  curl = curl_easy_init();
+
+  curl_easy_setopt(curl, CURLOPT_URL, "http://127.0.0.1:9998");
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, readtest);
+  curl_easy_cleanup(curl);
+  
+  std::cout << "TEST" << std::endl;
+
+  
+  return 0;
 }
